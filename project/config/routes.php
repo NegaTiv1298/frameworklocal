1cfb95d8c62e1da2f3ac0356f986425d579e95a2
NegaTiv1298 <levkristi@i.ua>
2021-05-16 23:13:40 +0300

    OOP.Less.94.3

--- project/config/routes.php
+++ project/config/routes.php
@@ -6,6 +6,8 @@
         new Route('/act1/', 'test', 'act1'),
         new Route('/act2/', 'test', 'act2'),
         new Route('/act3/', 'test', 'act3'),
-        new Route('/nums/:n1/:n2/:n3/', 'num', 'sum')
+        new Route('/nums/:n1/:n2/:n3/', 'num', 'sum'),
+        new Route('/user/:id/', 'user', 'show'),
+        new Route('/user/:id/:key/', 'user', 'info')
 	];
 	
