ba0bc352d61b29e69405738c5cb0618669e9299b
NegaTiv1298 <levkristi@i.ua>
2021-05-16 22:30:53 +0300

    OOP.Less.93

--- project/controllers/TestController.php
+++ project/controllers/TestController.php
@@ -1,3 +1,19 @@
 <?php
 namespace Project\Controllers;
 use \Core\Controller;
+
+class TestController extends Controller
+{
+    public function act1()
+    {
+        echo '1';
+    }
+    public function act2()
+    {
+        echo '2';
+    }
+    public function act3()
+    {
+        echo '3';
+    }
+}
