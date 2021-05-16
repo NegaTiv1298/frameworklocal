7719820fcf4cd04354804532dceec5611fea46e2
NegaTiv1298 <levkristi@i.ua>
2021-05-16 22:49:47 +0300

    OOP.Less.94.1

--- project/config/routes.php
+++ project/config/routes.php
@@ -5,6 +5,7 @@
 		new Route('/hello/', 'hello', 'index'), // роут для приветственной страницы, можно удалить
         new Route('/act1/', 'test', 'act1'),
         new Route('/act2/', 'test', 'act2'),
-        new Route('/act3/', 'test', 'act3')
+        new Route('/act3/', 'test', 'act3'),
+        new Route('/nums/:n1/:n2/:n3/', 'num', 'sum')
 	];
 	
